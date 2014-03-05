b260debfa22af3c928f2fcf1575175dcaf056bee
richard cohen <richard.cohen@comhem.se>
2014-03-05 21:52:00 +0100

    Even methods can be used as objects

--- chapter5 classes/grouping_objects.rb
+++ chapter5 classes/grouping_objects.rb
@@ -51,9 +51,23 @@ r1 = Rectangle.new(3, 4)
 puts r1.perimeter
 puts r1.area
 
+
+# So, to summarize, the data an object contains is what it is and its methods 
+# are what it can do. Implicit in this definition is the 
+# fact that the abilities of an object are limited to the methods it exposes.
+
 puts "-----------\n"
 puts 1.next
 
+
+# Methods aren't exempt from Ruby's "everything is an object" rule. 
+# This means that the methods exposed by any object are themselves objects,
+# and yes, you can use them as such.
+
+# All objects in Ruby expose the eponymous method method that can be used to get hold of any of its methods as an object.
+
+puts "-----------\n"
+puts 1.method("next")
 
 
 
