aca5cb11ba346fb7f3786fe62b048a55ee3e9b83
richard cohen <richard.cohen@comhem.se>
2014-03-05 22:58:36 +0100

    Calling a method with 2 parameters

---
+++ chapter6/chapter6_methods.rb
@@ -0,0 +1,15 @@
+## Chapter 6 Methods
+
+puts "---------------------------------"
+puts "  6.2 Calling a method           "
+puts "---------------------------------"
+
+# We'll expand this and talk about the ways in which methods can accept more than one parameter.
+
+puts "-----------\n"
+
+def add(a_number, another_number)
+  a_number + another_number
+end
+
+puts add(1, 2)
