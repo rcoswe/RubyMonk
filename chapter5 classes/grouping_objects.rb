09de93ea9a564d1703ed72bda785c5a94cb2608f
richard cohen <richard.cohen@comhem.se>
2014-03-05 21:28:15 +0100

    How to create a class, make object and call methods

--- chapter5 classes/grouping_objects.rb
+++ chapter5 classes/grouping_objects.rb
@@ -26,6 +26,36 @@ puts "---------------------------------"
 puts 1.class.class
 
 puts "---------------------------------"
-puts "                                 "
+puts "> Building your own classes      "
 puts "---------------------------------"
+
+# How to build a simple class
+# and create an object and call methods
+
+class Rectangle
+  def initialize(length, breadth)
+    @length = length
+    @breadth = breadth
+  end
+
+  def perimeter
+    2 * (@length + @breadth)
+  end
+
+  def area
+    @length * @breadth
+  end
+end
+
+r1 = Rectangle.new(3, 4)
+puts r1.perimeter
+puts r1.area
+
+
+
+
+
+
+
+
 
