6758f496dcad7a0709e76eba4857db0a4baae429
richard cohen <richard.cohen@comhem.se>
2014-03-10 20:05:52 +0100

    Using the splat operator

--- chapter6/chapter6_methods.rb
+++ chapter6/chapter6_methods.rb
@@ -44,3 +44,33 @@ end
 
 puts say_hello("Richard Cohen")
 puts say_hello
+
+# The list of parameters passed to an object is, in fact, available as a list. 
+# To do this, we use what is called the splat operator - which is just an asterisk (*).
+# The splat operator is used to handle methods which have a variable parameter list. 
+# Let's use it to create an add method that can handle any number of parameters.
+
+puts "-----------\n"
+
+def add(*numbers)
+  numbers.inject(0) { |sum, number| sum + number }
+end
+
+puts add(1)
+puts add(1, 2)
+puts add(1, 2, 3)
+puts add(1, 2, 3, 4)
+
+# The splat operator works both ways - you can use it to convert arrays to parameter lists
+# as easily as we just converted a parameter list to an array.
+# I'll show you how we can splat an array of three numbers into a parameter list so that it
+# works with one of the examples from earlier in this lesson that accepts exactly three parameters.
+
+
+puts "-----------\n"
+def add(a_number, another_number, yet_another_number)
+  a_number + another_number + yet_another_number
+end
+
+numbers_to_add = [1, 2, 3] # Without a splat, this is just one parameter
+puts add(*numbers_to_add)  # Try removing the splat just to see what happens
