0ea7f6459f42b7c7066fd0c8fe743d891a6d4864
richard cohen <richard.cohen@comhem.se>
2014-03-10 20:32:52 +0100

    Naming parameters and using hash at the end of the parameter list

--- chapter6/chapter6_methods.rb
+++ chapter6/chapter6_methods.rb
@@ -100,4 +100,23 @@ end
 
 puts introduction(62, "male", "richard", "benjamin", "cohen")
 
+# Naming parameters
+# This last section on method invocation is easiest demonstrated, then explained. 
+# Pay careful attention to the invocation of the add method in the example below. 
+# Look at how neatly we are able to pass configuration options to the method; 
+# the user of the add method gets to decide if the absolute value should be returned 
+# and if rounding should happen.
+puts "-----------\n"
+
+def add(a_number, another_number, options = {})
+  sum = a_number + another_number
+  sum = sum.abs if options[:absolute]
+  sum = sum.round(options[:precision]) if options[:round]
+  sum
+end
+
+puts add(1.0134, -5.568)
+puts add(1.0134, -5.568, absolute: true)
+puts add(1.0134, -5.568, absolute: true, round: true, precision: 2)
+
 
